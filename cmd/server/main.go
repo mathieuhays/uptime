ddc1f10c0f8c39ac1bae9ebedd4a72fc148d880a
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-07-21 02:27:56 +0200

    setup server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"github.com/mathieuhays/uptime"
 	"log"
+	"net/http"
 )
 
 func main() {
-	log.Println("hello there")
+	const addr = "localhost:8080"
+	server, err := uptime.NewServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Starting server on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, server))
 }

c5e607cfd53deec63ef91a70cd2ef12a0de95330
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-09-13 01:29:20 +0200

    website: add healthcheck chart

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ Simple tool to monitor multiple URLs
 - [ ] Website View: add option to toggle cache bypass setting?
 - [ ] Website View: add option to disable crawling
 - [ ] Unit test crawler
+- [ ] Website View: health-check historic, highlight non 200 codes
+- [ ] Website View: Show last known outage as a separate item
+- [x] Website View: health-check historic, add chartjs graph
 
 ## Ideas
 
 - [ ] Setup websockets for instant updates
 - [ ] Have an image attached to each website (automated). either screenshot, social media meta image or favicon. screenshot will probably require nodejs.
 - [ ] Website view: maybe infer page colour theme from target website?
+- [ ] website setting: response time budget, then color code health check historic to reflect it
--- internal/handlers/website.go
+++ internal/handlers/website.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/google/uuid"
 	"github.com/mathieuhays/uptime/internal/healthcheck"
@@ -8,6 +9,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Website(templ *template.Template, webRepo website.Repository, hcRepo healthcheck.Repository) http.Handler {
@@ -34,14 +36,40 @@ func Website(templ *template.Template, webRepo website.Repository, hcRepo health
 			return
 		}
 
+		var healthCheckChartData string
+		healthCheckChartDataBytes, err := json.Marshal(healthChecksToChartData(healthChecks))
+		if err == nil {
+			healthCheckChartData = string(healthCheckChartDataBytes)
+		}
+
 		if err = templ.ExecuteTemplate(writer, "website.gohtml", struct {
-			Website      website.Website
-			HealthChecks []healthcheck.HealthCheck
+			Website              website.Website
+			HealthChecks         []healthcheck.HealthCheck
+			HealthCheckChartData string
 		}{
-			Website:      *w,
-			HealthChecks: healthChecks,
+			Website:              *w,
+			HealthChecks:         healthChecks,
+			HealthCheckChartData: healthCheckChartData,
 		}); err != nil {
 			log.Printf("website view. error rendering template: %s", err)
 		}
 	})
+}
+
+type point struct {
+	X string `json:"x"`
+	Y int    `json:"y"`
+}
+
+func healthChecksToChartData(items []healthcheck.HealthCheck) []point {
+	points := []point{}
+
+	for _, item := range items {
+		points = append(points, point{
+			X: item.CreatedAt.Format(time.DateTime),
+			Y: int(item.ResponseTime.Milliseconds()),
+		})
+	}
+
+	return points
 }
